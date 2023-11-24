5d6bf4abc7b4c453e6af3466d257c42bca2db4c7
tatsuyaYAGI <yagiappwp@gmail.com>
2023-11-25 00:22:08 +0900

    S11-60

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,53 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import Herder from '../components/Header'
+import MemoListItem from '../components/MemoListItem'
+import CircleButton from '../components/CircleButton'
 
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-
-      <View style={styles.header}>
-        <View style={styles.headerInner}>
-          <Text style={styles.headerTitle}>Memo App</Text>
-          <Text style={styles.headerRight}>ログアウト</Text>
-        </View>
-      </View>
-
+      <Herder />
       <View>
-
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
+        <MemoListItem />
+        <MemoListItem />
+        <MemoListItem />
       </View>
-
-      <View>
-        <Text>+</Text>
-      </View>
+      <CircleButton>+</CircleButton>
     </View>
   )
 }
@@ -56,27 +21,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF'
-  },
-  header: {
-    backgroundColor: '#467FD3',
-    height: 104,
-    justifyContent: 'flex-end'
-  },
-  headerInner: {
-    alignItems: 'center'
-  },
-  headerRight: {
-    position: 'absolute',
-    right: 16,
-    bottom: 16,
-    color: 'rgba(255,255,255,0.7)'
-  },
-  headerTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    fontWeight: 'bold',
-    color: '#FFFFFF'
   }
 })
 
