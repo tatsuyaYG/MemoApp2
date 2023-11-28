6e81f634ccdaabc768aa31eec646347daea7edde
tatsuyaYAGI <yagiappwp@gmail.com>
2023-11-28 22:11:09 +0900

    セクション13_96

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,11 +1,13 @@
 import { View, StyleSheet } from 'react-native'
 import { router, useNavigation } from 'expo-router'
 import { useEffect } from 'react'
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
 import LogOutButton from '../../components/LogOutButton'
+import { db, auth } from '../../config'
 
 const handlePress = (): void => {
   router.push('/memo/create')
@@ -17,6 +19,17 @@ const List = (): JSX.Element => {
     navigation.setOptions({
       headerRight: () => { return <LogOutButton /> }
     })
+  }, [])
+  useEffect(() => {
+    if (auth.currentUser === null) { return }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    const q = query(ref, orderBy('updatedAt', 'desc'))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log('memo', doc.data())
+      })
+    })
+    return unsubscribe
   }, [])
   return (
     <View style={styles.container}>
