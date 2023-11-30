1ab6984acd06b796ca4faffe70737f3eae23ae39
tatsuyaYAGI <yagiappwp@gmail.com>
2023-12-01 00:15:49 +0900

    セクション13_99

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,6 +1,6 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList } from 'react-native'
 import { router, useNavigation } from 'expo-router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListItem'
@@ -8,12 +8,14 @@ import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
 import LogOutButton from '../../components/LogOutButton'
 import { db, auth } from '../../config'
+import { type Memo } from '../../../types/memo'
 
 const handlePress = (): void => {
   router.push('/memo/create')
 }
 
 const List = (): JSX.Element => {
+  const [memos, setMemos] = useState<Memo[]>([])
   const navigation = useNavigation()
   useEffect(() => {
     navigation.setOptions({
@@ -25,19 +27,26 @@ const List = (): JSX.Element => {
     const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
     const q = query(ref, orderBy('updatedAt', 'desc'))
     const unsubscribe = onSnapshot(q, (snapshot) => {
+      const remoteMemos: Memo[] = []
       snapshot.forEach((doc) => {
         console.log('memo', doc.data())
+        const { bodyText, updatedAt } = doc.data()
+        remoteMemos.push({
+          id: doc.id,
+          bodyText,
+          updatedAt
+        })
       })
+      setMemos(remoteMemos)
     })
     return unsubscribe
   }, [])
   return (
     <View style={styles.container}>
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
+      <FlatList
+        data={memos}
+        renderItem={({ item }) => <MemoListItem memo={item} />}
+      />
       <CircleButton onPress={handlePress}>
         <Icon name='plus' size={40} color='#ffffff'/>
       </CircleButton>
